9ee206c4d86b4cf884cf0a4f64e351d2d0bb34cd
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2023-01-04 13:52:09 +0000

    add paths

---
+++ util_mypaths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+path_to_constants = Path.home() / "scripts" / "constants"
+path_to_data_umserve = Path.home().parent.parent / "data" / "mz355"
+path_to_datasets = Path.home() / "datasets" / "zamyatina2023a_signatures_of_wind_driven_chemistry_on_hot_jupiters"
+path_to_observations = Path.home() / "observations"
+path_to_processed = Path.home() / "processed"
+path_to_results = Path.home() / "results" / "zamyatina2023a"
